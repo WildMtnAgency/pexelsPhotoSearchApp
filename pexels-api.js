860792a147cf9f4638ff09c9ab4c93765a46bb95
Kim Weincek <kim@wildmountain.agency>
2022-12-06 10:32:57 -0700

    minor style changes in search-bar

---
+++ pexels-api.js
@@ -0,0 +1,33 @@
+var photoFeed = document.getElementById('photo-feed');
+var searchForm = document.getElementById('search-form');
+searchForm.addEventListener('submit', function(e){
+	e.preventDefault();
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			// Typical action to be performed when the document is ready:
+			var res = JSON.parse(xhttp.responseText);
+			//JSON parses response text from api
+			console.log(res);
+			//selects photos object from res and returns each photo | 0-14 | total 15
+			var photoData = res.photos.map(function(photo){
+				console.log(photo.id)
+				return photo
+			});
+
+			photoFeed.innerHTML = '';
+			photoData.forEach(function(photo) {
+				var photoDiv = document.createElement('div');
+				photoDiv.classList.add('photo-div');
+				photoDiv.innerHTML = `
+					<img class="pSize" src="${photo.src.portrait}">
+				`;
+				photoFeed.appendChild(photoDiv)
+			});
+		}
+	};
+	var searchQuery = document.querySelector('#search-bar').value;
+	xhttp.open("GET", `https://api.pexels.com/v1/search?query=${searchQuery}`, true);
+	xhttp.setRequestHeader('Authorization', '563492ad6f91700001000001fee5f61edff643339f6063a86c85c706');
+	xhttp.send();
+});
